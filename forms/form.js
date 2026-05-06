21b54c332161c8a2dc19c895108dd39079644f3a
yansu-blip <yan.su@un.org>
2026-05-06 15:14:26 +0200

    Update form.js

--- forms/form.js
+++ forms/form.js
@@ -1,11 +1,8 @@
 fd.rendered(async function () {
     try {
-        await OHCHR.initializeDropdowns();
-
-        OHCHR.initRichTextEditors();
-
+        await OHCHR.initializeDropdowns(window.OHCHR_FORM_CONFIG?.dropdowns);
+        OHCHR.initRichTextEditors(window.OHCHR_FORM_CONFIG?.richTextEditors);        
         initRichTextCleaners(window.OHCHR_FORM_CONFIG?.richTextCleaners || []);
-
         console.log('Form initialized successfully.');
     } catch (error) {
         console.error('Error initializing Statements form:', error);
