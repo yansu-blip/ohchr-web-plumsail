bfe9052dc3c08723c1896b37f888276c27089d08
yansu-blip <yan.su@un.org>
2026-05-07 17:09:00 +0200

    Pin loader example to taxonomy URL guard commit

--- examples/statements-form-loader.js
+++ examples/statements-form-loader.js
@@ -15,9 +15,10 @@ fd.rendered(async function() {
         window.fd = fd;
 
         // Pinned to the PR branch commit for testing. Use @main after this branch is merged.
-        const baseUrl = 'https://cdn.jsdelivr.net/gh/yansu-blip/ohchr-web-plumsail@c85d26f261b8349a1a0327f2db58cd52150e0dcd';
+        const baseUrl = 'https://cdn.jsdelivr.net/gh/yansu-blip/ohchr-web-plumsail@de126ffffa0505901617b38a98e9595a1d3388db';
 
         window.OHCHR_CONFIG = {
+            // Replace this placeholder with the full Power Automate HTTP trigger URL.
             FLOW_URL: '<POWER_AUTOMATE_FLOW_URL>'
         };
 
